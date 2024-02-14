7448a9c1ef73c60898ab143e0c0c1b3f03f39802
Joseph Marchbanks <jmarchbanks2@cnm.edu>
2024-02-14 16:31:32 -0700

    Created static post element, interaction bar still needs work

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,10 @@
+import {Posts} from "@/app/shared/Posts";
+
 export default function Home() {
     return (
+        <>
         <h1 className="text-3xl font-bold underline">Is this thing on?</h1>
+        <Posts/>
+        </>
     )
 }
