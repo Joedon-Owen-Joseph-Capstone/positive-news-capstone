45580bd7c2b11df8a008e00d1eaf15ba08c841f1
Owen Scarpa <154032546+odscarpa@users.noreply.github.com>
2024-02-16 10:26:58 -0700

    copied about section two more times and added all functional links

--- frontend/src/app/about-us/page.tsx
+++ frontend/src/app/about-us/page.tsx
@@ -1,40 +1,145 @@
 export default function About() {
     return (
         <>
-            <section className="py-18 text-black">
+            {/*about joseph*/}
+            <section id={"joseph"} className="py-18 text-black">
                 <h1 className="text-4xl font-bold text-center pt-10 italic">ABOUT PNN</h1>
                 <div className="md:flex gap-10 mx-auto max-md:space-y-12 p-16 flex-4">
                     <div className="basis-1/6 flex-shrink-0 justify-center max-md:px-16">
-                        <img src="https://placekitten.com/g/500/500" alt="cat" height="500" width="500" className="shadow-2xl rounded-lg"/>
+                        <img src="https://placekitten.com/g/500/500" alt="cat" height="500" width="500"
+                             className="shadow-2xl rounded-lg"/>
                     </div>
-                    <div className="basis-5/6 max-md-8">
-                        <h3 className="font-bold text-xl italic">Owner Name</h3>
-                        <p className="md:py-2 mr-32 mt-10">Now that there is the Tec-9, a crappy spray gun from South Miami.
+                    <div className="basis-5/6 max-md-8  bg-[]">
+                        <h3 className="font-bold text-xl italic text-center">Joseph Marchbanks</h3>
+                        <p className="md:py-2 mt-4">Now that there is the Tec-9, a crappy spray gun from South
+                            Miami.
                             This gun is advertised as the most popular gun in American crime. Do you believe that shit?
                             It actually says that in the little book that comes with it:
                             the most popular gun in American crime. Like they're actually proud of that shit.</p>
-                        <p className="md:py-2 mr-32"> the path of the righteous man is beset on all sides by the iniquities
+                        <p className="md:py-2"> the path of the righteous man is beset on all sides by the
+                            iniquities
                             of
                             the selfish and the tyranny of evil men. then they show that show to the people who make
                             shows,
                             and on the strength of that one show they decide if they're going to make more shows. they
                             called me mr glass. </p>
                     </div>
-                    <div>
-                        <h4 className="font-bold italic">Contact</h4>
-                        <a href=""><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
-                             className="bi bi-github mb-2.5" viewBox="0 0 16 16">
-                            <path
-                                d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8"/>
-                        </svg></a>
+                    <div className={"mt-20"}>
+                        <h4 className="font-bold italic">Contact Joseph</h4>
+                        <div className={"inline-flex px-10 mx-auto"}>
+                        <a href="https://github.com/JPHMarchB">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
+                                 className="bi bi-github mb-2.5 hover:shadow-2xl" viewBox="0 0 16 16">
+                                <path
+                                    d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8"/>
+                            </svg>
+                        </a>
 
-                        <a href=""><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="blue"
-                             className="bi bi-linkedin" viewBox="0 0 16 16">
-                            <path
-                                d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854zm4.943 12.248V6.169H2.542v7.225zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248S2.4 3.226 2.4 3.934c0 .694.521 1.248 1.327 1.248zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016l.016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225z"/>
-                        </svg></a>
+                        <a href="https://www.linkedin.com/in/joseph-marchbanks-508a362a4/">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="blue"
+                                 className="bi bi-linkedin hover:shadow-2xl" viewBox="0 0 16 16">
+                                <path
+                                    d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854zm4.943 12.248V6.169H2.542v7.225zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248S2.4 3.226 2.4 3.934c0 .694.521 1.248 1.327 1.248zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016l.016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225z"/>
+                            </svg>
+                        </a>
+                        </div>
+                        <a href="mailto:josephmarchbanks599@gmail.com"><p
+                            className={"hover:text-purple-700 hover:underline"}>josephmarchbanks599@gmail.com</p></a>
+                    </div>
+                </div>
+            </section>
 
-                        <a href="mailto:odscarpa512@gmail.com"><p>email@gmail.com</p></a>
+            {/*about owen*/}
+            <section id={"owen"} className="py-18 text-black">
+                <div className="md:flex gap-10 mx-auto max-md:space-y-12 p-16 flex-4">
+                    <div className="basis-1/6 flex-shrink-0 justify-center max-md:px-16">
+                        <img src="https://placekitten.com/g/500/500" alt="cat" height="500" width="500"
+                             className="shadow-2xl rounded-lg"/>
+                    </div>
+                    <div className="basis-5/6 max-md-8">
+                        <h3 className="font-bold text-xl italic text-center">Owen Scarpa</h3>
+                        <p className="md:py-2 mt-4">Now that there is the Tec-9, a crappy spray gun from South
+                            Miami.
+                            This gun is advertised as the most popular gun in American crime. Do you believe that shit?
+                            It actually says that in the little book that comes with it:
+                            the most popular gun in American crime. Like they're actually proud of that shit.</p>
+                        <p className="md:py-2"> the path of the righteous man is beset on all sides by the
+                            iniquities
+                            of
+                            the selfish and the tyranny of evil men. then they show that show to the people who make
+                            shows,
+                            and on the strength of that one show they decide if they're going to make more shows. they
+                            called me mr glass. </p>
+                    </div>
+                    <div className={"mt-20"}>
+                        <h4 className="font-bold italic">Contact Owen</h4>
+                        <div className={"inline-flex px-10 mx-auto"}>
+                        <a href="https://github.com/odscarpa">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
+                                 className="bi bi-github mb-2.5 hover:shadow-2xl" viewBox="0 0 16 16">
+                                <path
+                                    d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8"/>
+                            </svg>
+                        </a>
+
+                        <a href="https://www.linkedin.com/in/owen-scarpa-baaa802a4/">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="blue"
+                                 className="bi bi-linkedin hover:shadow-2xl" viewBox="0 0 16 16">
+                                <path
+                                    d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854zm4.943 12.248V6.169H2.542v7.225zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248S2.4 3.226 2.4 3.934c0 .694.521 1.248 1.327 1.248zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016l.016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225z"/>
+                            </svg>
+                        </a>
+                        </div>
+                        <a href="mailto:odscarpa512@gmail.com"><p
+                            className={"hover:text-purple-700 hover:underline"}>odscarpa512@gmail.com</p></a>
+                    </div>
+                </div>
+            </section>
+
+            {/*about joedon*/}
+            <section id={"joedon"} className="py-18 text-black">
+                <div className="md:flex gap-10 mx-auto max-md:space-y-12 p-16 flex-4">
+                    <div className="basis-1/6 flex-shrink-0 justify-center max-md:px-16">
+                        <img src="https://placekitten.com/g/500/500" alt="cat" height="500" width="500"
+                             className="shadow-2xl rounded-lg"/>
+                    </div>
+                    <div className="basis-5/6 max-md-8">
+                        <h3 className="font-bold text-xl italic text-center">Joedon Mconnell</h3>
+                        <p className="md:py-2 mt-4">Now that there is the Tec-9, a crappy spray gun from South
+                            Miami.
+                            This gun is advertised as the most popular gun in American crime. Do you believe that shit?
+                            It actually says that in the little book that comes with it:
+                            the most popular gun in American crime. Like they're actually proud of that shit.</p>
+                        <p className="md:py-2"> the path of the righteous man is beset on all sides by the
+                            iniquities
+                            of
+                            the selfish and the tyranny of evil men. then they show that show to the people who make
+                            shows,
+                            and on the strength of that one show they decide if they're going to make more shows. they
+                            called me mr glass. </p>
+                    </div>
+                    <div className={"mt-20"}>
+                        <h4 className="font-bold italic">Contact Joedon</h4>
+                        <div className={"inline-flex px-10 mx-auto"}>
+                        <a href="https://github.com/JoedyMcconn">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
+                                 className="bi bi-github mb-2.5 hover:shadow-2xl" viewBox="0 0 16 16">
+                                <path
+                                    d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8"/>
+                            </svg>
+                        </a>
+
+                        <a href="hhttps://www.linkedin.com/in/joedon-mcconnell-381a8013a/">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="blue"
+                                 className="bi bi-linkedin hover:shadow-2xl" viewBox="0 0 16 16">
+                                <path
+                                    d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854zm4.943 12.248V6.169H2.542v7.225zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248S2.4 3.226 2.4 3.934c0 .694.521 1.248 1.327 1.248zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016l.016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225z"/>
+                            </svg>
+                        </a>
+                        </div>
+
+                        <a href="mailto:jdmconnell@gmail.com"><p
+                            className={"hover:text-purple-700 hover:underline"}>jdmconnell@gmail.com</p></a>
                     </div>
                 </div>
             </section>
