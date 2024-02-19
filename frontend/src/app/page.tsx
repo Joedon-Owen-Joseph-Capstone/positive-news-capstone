c30014274e7579ec53fe5d0c3c8f908b1f8b9ece
Joseph Marchbanks <jmarchbanks2@cnm.edu>
2024-02-19 13:38:25 -0700

    adjusted sizing for mobile optimization on history and post sections

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -4,8 +4,8 @@ export default function Home() {
     return (
         <>
             <section className='my-24'>
-                <h1 className="text-6xl font-bold text-center text-white italic pb-24">POSITIVE NEWS NETWORK</h1>
-                <div className="carousel w-full">
+                <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold text-center text-white italic pb-24">POSITIVE NEWS NETWORK</h1>
+                <div className="carousel w-full min-h-72">
                     <div id="slide1" className="carousel-item relative w-full">
                         <img src="https://daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.jpg"
                              className="w-full"/>
@@ -47,7 +47,7 @@ export default function Home() {
             <section className='my-24'>
                 <div className="container mx-auto">
                     <div className='flex justify-between'>
-                        <h2 className='text-5xl'>Popular U.S.</h2>
+                        <h2 className='text-3xl md:text-4xl lg:text-5xl'>Popular U.S.</h2>
                         <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="white"
                              className="bi bi-filter-right" viewBox="0 0 16 16">
                             <path
@@ -59,12 +59,12 @@ export default function Home() {
             </section>
 
             <section className='bg-gray-500 p-10'>
-                <h2 className='text-center text-5xl'>Today in History</h2>
-                <div className='flex justify-center items-center gap-20 p-10'>
-                    <img className='image-full' src={"https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png"} alt={'history pic'}/>
+                <h2 className='text-center text-3xl md:text-4xl lg:text-5xl'>Today in History</h2>
+                <div className='lg:flex justify-center items-center gap-20 p-5 lg:p-10'>
+                    <img className='w-[40rem]' src={"https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png"} alt={'history pic'}/>
                     <div>
-                        <h3 className='text-4xl'>Title</h3>
-                        <p className='text-2xl'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque hendrerit tincidunt nulla fermentum faucibus. Integer consectetur est vel pharetra viverra. Donec commodo mi nec ligula pharetra mattis. Proin faucibus magna ut elit efficitur efficitur. Aliquam vel quam ut lectus tincidunt molestie a ac risus. Donec ut ornare ante. In quis orci dui. </p>
+                        <h3 className='text-2xl md:text-3xl lg:text-4xl pt-5 lg:pt-0'>Title</h3>
+                        <p className='text-lg md:text-xl lg:text-2xl'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque hendrerit tincidunt nulla fermentum faucibus. Integer consectetur est vel pharetra viverra. Donec commodo mi nec ligula pharetra mattis. Proin faucibus magna ut elit efficitur efficitur. Aliquam vel quam ut lectus tincidunt molestie a ac risus. Donec ut ornare ante. In quis orci dui. </p>
                     </div>
                 </div>
 
@@ -73,7 +73,7 @@ export default function Home() {
             <section className='my-24'>
                 <div className="container mx-auto">
                     <div className='flex justify-between'>
-                        <h2 className='text-5xl'>Popular World</h2>
+                        <h2 className='text-3xl md:text-4xl lg:text-5xl'>Popular World</h2>
                         <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="white"
                              className="bi bi-filter-right" viewBox="0 0 16 16">
                             <path
@@ -84,9 +84,9 @@ export default function Home() {
                 </div>
             </section>
 
-            <section className='flex justify-between items-baseline border-t-4 border-gray-300 p-20'>
-                <h2 className='text-5xl'>PNN</h2>
-                <p className='text-2xl hover:underline'>Back to the Top</p>
+            <section className='flex justify-between items-baseline border-t-4 border-gray-300 p-10 lg:p-20'>
+                <h2 className='text-3xl md:text-4xl lg:text-5xl'>PNN</h2>
+                <p className='text-lg md:text-xl lg:text-2xl hover:underline'>Back to the Top</p>
             </section>
         </>
     )
