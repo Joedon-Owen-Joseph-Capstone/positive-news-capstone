15acdf6467a446e7c4f9c0315e9df657268eb314
Joseph Marchbanks <jmarchbanks2@cnm.edu>
2024-02-20 11:02:03 -0700

    Improved code documentation of footer component with comments explaining code

--- frontend/src/app/shared/Navigation.tsx
+++ frontend/src/app/shared/Navigation.tsx
@@ -3,80 +3,105 @@ import {JSX} from 'react'
 
 export function Navigation() : JSX.Element {
     return(
-        <nav>
-            <div className="navbar bg-gradient-to-br from-[#0D0B33] to-[#52489F] text-white p-5 rounded-b">
+        <>
 
-                <div className="flex-1">
-                    <a className="btn btn-ghost text-2xl md:text-3xl lg:text-4xl">PNN</a>
-                </div>
+            {/* Nav bar content */}
+            <nav>
 
-                <div className="flex-none gap-2">
+                {/* Nav bar container */}
+                <div className="navbar bg-gradient-to-br from-[#0D0B33] to-[#52489F] text-white p-5 rounded-b">
 
-                    <div>
-                        <input type="text" placeholder="Search"
-                               className="input input-bordered w-24 md:w-auto bg-[#e2e1f9] text-black"/>
+                    {/* PNN logo/Home button */}
+                    <div className="flex-1">
+                        <a className="btn btn-ghost text-2xl md:text-3xl lg:text-4xl">PNN</a>
                     </div>
 
-                    <div className="flex-none">
-                        <ul className="menu menu-horizontal z-50">
-                            <li>
-                                <details>
-                                    <summary className="lg:hidden text-3xl focus:outline-2 cursor-pointer">
-                                        &#9776;
-                                    </summary>
-                                    <ul className="bg-white italic p-2 rounded-t-none text-black">
-                                        <li>
-                                            <div className="dropdown dropdown-end">
+                    <div className="flex-none gap-2">
 
-                                                <div tabIndex={0} role="button"
-                                                     className="btn btn-ghost btn-circle avatar">
-                                                    <div className="w-10 rounded-full">
-                                                        <img alt="Tailwind CSS Navbar component"
-                                                             src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"/>
-                                                    </div>
-                                                </div>
-
-                                                <ul tabIndex={0}
-                                                    className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52 text-black">
-                                                    <li><a className="justify-between">Profile</a></li>
-                                                    <li><a>Logout</a></li>
-                                                </ul>
-                                            </div>
-                                        </li>
-                                        <li><a>About PNN</a></li>
-                                        <li><a>U.S.</a></li>
-                                        <li><a>World</a></li>
-                                    </ul>
-                                </details>
-                            </li>
-                        </ul>
-                    </div>
-
-                    <div className='hidden lg:flex'>
-                        <ul className="menu menu-horizontal px-1 text-lg">
-                            <li className='hover:underline'><a>About PNN</a></li>
-                            <li className='hover:underline'><a>U.S.</a></li>
-                            <li className='hover:underline'><a>World</a></li>
-                        </ul>
-                    </div>
-
-                    <div className="dropdown dropdown-end hidden lg:flex">
-
-                        <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
-                            <div className="w-10 rounded-full">
-                                <img alt="Tailwind CSS Navbar component"
-                                     src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"/>
-                            </div>
+                        {/* Search bar */}
+                        <div>
+                            <input type="text" placeholder="Search"
+                                   className="input input-bordered w-24 md:w-auto bg-[#e2e1f9] text-black"/>
                         </div>
 
-                        <ul tabIndex={0}
-                            className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52 text-black">
-                            <li><a className="justify-between">Profile</a></li>
-                            <li><a>Logout</a></li>
-                        </ul>
+                        {/* Dropdown button content */}
+                        <div className="flex-none">
+
+                            {/* nav bar list */}
+                            <ul className="menu menu-horizontal z-50">
+                                <li>
+                                    <details>
+
+                                        {/* Drop down button */}
+                                        <summary className="lg:hidden text-3xl focus:outline-2 cursor-pointer">
+                                            &#9776;
+                                        </summary>
+
+                                        {/* Drop down links */}
+                                        <ul className="bg-white italic p-2 rounded-t-none text-black">
+
+                                            {/* Profile button */}
+                                            <li>
+                                                <div className="dropdown dropdown-end">
+
+                                                    <div tabIndex={0} role="button"
+                                                         className="btn btn-ghost btn-circle avatar">
+                                                        <div className="w-10 rounded-full">
+                                                            <img alt="Tailwind CSS Navbar component"
+                                                                 src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"/>
+                                                        </div>
+                                                    </div>
+
+                                                    {/* Profile button links */}
+                                                    <ul tabIndex={0}
+                                                        className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52 text-black">
+                                                        <li><a className="justify-between">Profile</a></li>
+                                                        <li><a>Logout</a></li>
+                                                    </ul>
+                                                </div>
+                                            </li>
+
+                                            {/* Other drop down button links */}
+                                            <li><a>About PNN</a></li>
+                                            <li><a>U.S.</a></li>
+                                            <li><a>World</a></li>
+                                        </ul>
+                                    </details>
+                                </li>
+                            </ul>
+                        </div>
+
+                        {/* Regular nav bar links */}
+                        <div className='hidden lg:flex'>
+
+                            {/* Nav bar links */}
+                            <ul className="menu menu-horizontal px-1 text-lg">
+                                <li className='hover:underline'><a>About PNN</a></li>
+                                <li className='hover:underline'><a>U.S.</a></li>
+                                <li className='hover:underline'><a>World</a></li>
+                            </ul>
+                        </div>
+
+                        {/* Profile button */}
+                        <div className="dropdown dropdown-end hidden lg:flex">
+
+                            <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+                                <div className="w-10 rounded-full">
+                                    <img alt="Tailwind CSS Navbar component"
+                                         src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"/>
+                                </div>
+                            </div>
+
+                            {/* Profile button links */}
+                            <ul tabIndex={0}
+                                className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52 text-black">
+                                <li><a className="justify-between">Profile</a></li>
+                                <li><a>Logout</a></li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </nav>
+            </nav>
+        </>
     )
 }
