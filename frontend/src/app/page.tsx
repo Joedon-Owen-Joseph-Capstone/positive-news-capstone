6d15fb450ba6afd92cf3c923b3ba22e880672545
Joseph Marchbanks <jmarchbanks2@cnm.edu>
2024-02-20 16:15:54 -0700

    Added additional styling and color to home page along with new fonts, removed section at the bottom of the page to add to footer

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
                     {/* Carousel image 1 */}
                     <div id="slide1" className="carousel-item relative w-full">
                         <img src="https://daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.jpg"
-                             className="w-full" alt='slide 1 image'/>
+                             className="image-full" alt='slide 1 image'/>
                         <div
                             className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
                             <a href={"#slide4"} className="btn btn-circle">❮</a>
@@ -60,13 +60,13 @@ export default function Home() {
             </section>
 
             {/* U.S. posts */}
-            <section className='my-24'>
+            <section className='mt-28 lg:mt-56'>
                 <div className="container mx-auto">
 
                     {/* Title and filter */}
                     <div className='flex justify-between'>
-                        <h2 className='text-3xl md:text-4xl lg:text-5xl'>Popular U.S.</h2>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="white"
+                        <h2 className='font-custom text-3xl md:text-4xl lg:text-5xl'>Popular U.S.</h2>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="black"
                              className="bi bi-filter-right" viewBox="0 0 16 16">
                             <path
                                 d="M14 10.5a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0 0 1h3a.5.5 0 0 0 .5-.5m0-3a.5.5 0 0 0-.5-.5h-7a.5.5 0 0 0 0 1h7a.5.5 0 0 0 .5-.5m0-3a.5.5 0 0 0-.5-.5h-11a.5.5 0 0 0 0 1h11a.5.5 0 0 0 .5-.5"/>
@@ -77,32 +77,32 @@ export default function Home() {
             </section>
 
             {/* History section */}
-            <section className='bg-gray-500 p-10'>
-                <h2 className='text-center text-3xl md:text-4xl lg:text-5xl'>Today in History</h2>
+            <section className='bg-[#0e142a] text-white font-custom p-20 mt-28 lg:mt-56'>
+                <h2 className='text-center text-3xl md:text-4xl lg:text-5xl pb-5'>Today in History</h2>
 
                 {/* History content */}
-                <div className='lg:flex justify-center items-center gap-20 p-5 lg:p-10'>
+                <div className='lg:flex justify-center items-center gap-20'>
 
                     {/* History image */}
-                    <img className='w-[40rem]' src={"https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png"} alt={'history pic'}/>
+                    <img className='w-96' src={"https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png"} alt={'history pic'}/>
 
                     {/* History text */}
                     <div>
                         <h3 className='text-2xl md:text-3xl lg:text-4xl pt-5 lg:pt-0'>Title</h3>
-                        <p className='text-lg md:text-xl lg:text-2xl'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque hendrerit tincidunt nulla fermentum faucibus. Integer consectetur est vel pharetra viverra. Donec commodo mi nec ligula pharetra mattis. Proin faucibus magna ut elit efficitur efficitur. Aliquam vel quam ut lectus tincidunt molestie a ac risus. Donec ut ornare ante. In quis orci dui. </p>
+                        <p className='font-serif text-lg md:text-xl lg:text-2xl'>Article Description</p>
                     </div>
                 </div>
 
             </section>
 
             {/* World posts */}
-            <section className='my-24'>
+            <section className='mt-28 lg:mt-56'>
                 <div className="container mx-auto">
                     <div className='flex justify-between'>
 
                         {/* Title and filter */}
-                        <h2 className='text-3xl md:text-4xl lg:text-5xl'>Popular World</h2>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="white"
+                        <h2 className='font-custom text-3xl md:text-4xl lg:text-5xl'>Popular World</h2>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="black"
                              className="bi bi-filter-right" viewBox="0 0 16 16">
                             <path
                                 d="M14 10.5a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0 0 1h3a.5.5 0 0 0 .5-.5m0-3a.5.5 0 0 0-.5-.5h-7a.5.5 0 0 0 0 1h7a.5.5 0 0 0 .5-.5m0-3a.5.5 0 0 0-.5-.5h-11a.5.5 0 0 0 0 1h11a.5.5 0 0 0 .5-.5"/>
@@ -110,12 +110,6 @@ export default function Home() {
                     </div>
                     <Posts/>
                 </div>
-            </section>
-
-            {/* Page bottom content */}
-            <section className='flex justify-between items-baseline border-t-4 border-gray-300 p-10 lg:p-20'>
-                <h2 className='text-3xl md:text-4xl lg:text-5xl'>PNN</h2>
-                <p className='text-lg md:text-xl lg:text-2xl hover:underline'>Back to the Top</p>
             </section>
         </>
     )
