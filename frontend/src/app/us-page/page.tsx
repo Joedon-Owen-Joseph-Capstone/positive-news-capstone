2f7d8f78f75639512e041aff4d01e6cba1abdb07
Joseph Marchbanks <jmarchbanks2@cnm.edu>
2024-02-23 10:22:35 -0700

    Added background image and header to world page and made both US and world pages mobile friendly

--- frontend/src/app/us-page/page.tsx
+++ frontend/src/app/us-page/page.tsx
@@ -3,8 +3,11 @@ import {Posts} from "@/app/shared/Posts";
 export default function Us() {
     return (
         <>
-            <section className="bg-center bg-cover bg-fixed bg-[url('https://live.staticflickr.com/2920/14214064049_4fbe307724_b.jpg')]">
-                <h1 className='text-4xl md:text-5xl lg:text-6xl font-custom text-center text-white italic p-60'>US NEWS</h1>
+            <title>U.S. Page</title>
+            <section
+                className="bg-center bg-cover bg-fixed bg-[url('https://live.staticflickr.com/2920/14214064049_4fbe307724_b.jpg')]">
+                <h1 className='text-4xl md:text-5xl lg:text-6xl font-custom text-center text-white italic py-60'>US
+                    NEWS</h1>
             </section>
 
             <section>
--- frontend/src/app/world-page/page.tsx
+++ frontend/src/app/world-page/page.tsx
@@ -3,8 +3,12 @@ import {Posts} from "@/app/shared/Posts";
 export default function World() {
     return (
         <>
-            <title>Popular World</title>
-            <section className='mt-28 lg:mt-56'>
+            <title>World Page</title>
+            <section
+                className="bg-center bg-cover bg-fixed bg-[url('https://eoimages.gsfc.nasa.gov/images/imagerecords/149000/149388/iss066e025062_lrg.jpg')]">
+                <h1 className='text-4xl md:text-5xl lg:text-6xl font-custom text-center text-white italic py-60'>WORLD NEWS</h1>
+            </section>
+            <section>
                 <div className="container mx-auto">
 
                     {/* Title and filter */}
