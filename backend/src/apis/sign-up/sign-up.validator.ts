4cf2b7f9ba72eee3e3eddc562a5b912f246e0045
JoeDon McConnell <jmcconnell@cnm.edu>
2024-02-27 09:41:34 -0700

    added sign-up.validator

---
+++ backend/src/apis/sign-up/sign-up.validator.ts
@@ -0,0 +1,16 @@
+import {z} from 'zod'
+import {PrivateProfileSchema} from "../profile/profile.validator";
+
+export const SignUpProfileSchema = PrivateProfileSchema
+    .omit({ profileId: true, profileHash: true, profileActivationToken: true, profileImageUrl: true, profileAbout: true })
+    .extend({
+        profilePasswordConfirm: z.string()
+            .min(8, { message: 'please provide a valid password (min 8 characters)' })
+            .max(32, { message: 'please provide a valid password (max 32 characters)' }),
+        profilePassword: z.string()
+            .min(8, { message: 'please provide a valid password (min 8 characters)' })
+            .max(32, { message: 'please provide a valid password (max 32 characters)' })
+    })
+    .refine(data => data.profilePassword === data.profilePasswordConfirm, {
+        message: 'passwords do not match'
+    })
