97ef33dff7b121d69ccae772c7637014fbb046d9
Joseph Marchbanks <jmarchbanks2@cnm.edu>
2024-02-27 15:33:08 -0700

    Removed uncessary search function for profile name in profile  section in the back end

--- backend/src/apis/profile/profile.route.ts
+++ backend/src/apis/profile/profile.route.ts
@@ -1,8 +1,6 @@
 import {Router} from "express";
 import {
-    getPublicProfileByProfileIdController, getPublicProfileByProfileNameController,
-    getPublicProfilesByProfileNameController,
-    putProfileController
+    getPublicProfileByProfileIdController, putProfileController
 } from "./profile.controller";
 import {isLoggedInController} from "../../utils/controllers/isLoggedIn.controller";
 
@@ -15,10 +13,4 @@ router.route('/:profileId')
     .get(getPublicProfileByProfileIdController)
     .put(isLoggedInController, putProfileController)
 
-router.route('/profileNames/:profileName')
-    .get(getPublicProfilesByProfileNameController)
-
-router.route('/profileName/:profileName')
-    .get(getPublicProfileByProfileNameController)
-
-export const profileRoute = {basePath, router}
+export const profileRoute = { basePath, router }
