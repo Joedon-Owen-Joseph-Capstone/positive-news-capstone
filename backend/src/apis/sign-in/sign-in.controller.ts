2db3475a7cf246909432a54791528e4f1381f867
Joseph Marchbanks <jmarchbanks2@cnm.edu>
2024-02-28 09:25:31 -0700

    Simplified validator in sign-in

--- backend/src/apis/sign-in/sign-in.controller.ts
+++ backend/src/apis/sign-in/sign-in.controller.ts
@@ -24,7 +24,7 @@ export async function signInController (request: Request, response: Response): P
         const validationResult = signInProfileSchema.safeParse(request.body)
 
         // if the validation is unsuccessful, return a preformatted response to the client
-        if (validationResult.success !== true) {
+        if (!validationResult.success) {
             return zodErrorResponse(response, validationResult.error)
         }
 
