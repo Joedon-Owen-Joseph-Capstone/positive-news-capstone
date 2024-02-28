90cf4b9b9b67002c1ce7d263bcb32814f11f992c
Joseph Marchbanks <jmarchbanks2@cnm.edu>
2024-02-28 09:52:45 -0700

    Alphabetized profile table in sql

--- sql/project.sql
+++ sql/project.sql
@@ -13,10 +13,10 @@ CREATE TABLE IF NOT EXISTS tag(
 CREATE TABLE IF NOT EXISTS profile(
     profile_id UUID NOT NULL PRIMARY KEY,
     profile_activation_token CHAR(32),
-    profile_name VARCHAR(32) NOT NULL UNIQUE,
     profile_email VARCHAR(128) NOT NULL UNIQUE,
     profile_hash CHAR(97) NOT NULL UNIQUE,
-    profile_image_url VARCHAR(255)
+    profile_image_url VARCHAR(255),
+    profile_name VARCHAR(32) NOT NULL UNIQUE
 );
 
 CREATE TABLE IF NOT EXISTS article(
