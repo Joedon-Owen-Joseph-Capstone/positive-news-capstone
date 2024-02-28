edab66ecdcc52791071aa294d0dcc4343273df86
Joseph Marchbanks <jmarchbanks2@cnm.edu>
2024-02-28 15:04:22 -0700

    Added routes for sign in and sign out

--- backend/src/App.ts
+++ backend/src/App.ts
@@ -6,6 +6,8 @@ import session from 'express-session'
 import { createClient,  RedisClientType } from 'redis'
 import RedisStore from 'connect-redis'
 import {signUpRoute} from "./apis/sign-up/sign-up.route";
+import {signInRoute} from "./apis/sign-in/sign-in.route";
+import {signOutRoute} from "./apis/sign-out/sign-out.route";
 
 // The following class creates the app and instantiates the server
 export class App {
@@ -50,6 +52,9 @@ export class App {
     private routes (): void {
         this.app.use(indexRoute.basePath, indexRoute.router)
         this.app.use(signUpRoute.basePath, signUpRoute.router)
+        this.app.use(signInRoute.basePath, signInRoute.router)
+        this.app.use(signOutRoute.basePath, signOutRoute.router)
+
     }
 
     // starts the server and tells the terminal to post a message that the server is running and on what port
