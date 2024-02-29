734356373e7578eae07809fb76450f0ab07d71ee
Owen Scarpa <154032546+odscarpa@users.noreply.github.com>
2024-02-29 09:59:43 -0700

    re-wrote the validator others are still wacky.

--- backend/src/apis/article-tag/article-tag.validator.ts
+++ backend/src/apis/article-tag/article-tag.validator.ts
@@ -1,6 +1,15 @@
 import { z } from 'zod';
 
 export const ArticleTagSchema = z.object({
-    article_tag_article_id: z.string().uuid(),
-    article_tag_tag_id: z.string().uuid(),
+    article_tag_article_id: z.string({
+        required_error: 'please provide a valid articleTagArticleId',
+        invalid_type_error: 'articleTagArticleId is not the correct type'
+    })
+        .uuid({message: 'please provide a valid uuid for articleTagArticleId'}),
+
+    article_tag_tag_id: z.string({
+        required_error: "please provide a valid articleTagTagId",
+        invalid_type_error: "articleTagTagId is not the correct type"
+    })
+        .uuid({message: 'please provide a valid uuid for articleTagTagId'})
 });
