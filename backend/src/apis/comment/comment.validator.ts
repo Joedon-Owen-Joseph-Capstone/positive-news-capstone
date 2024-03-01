2795c55f58bd54da526a0d587e026751722dbc81
JoeDon McConnell <jmcconnell@cnm.edu>
2024-03-01 09:41:18 -0700

    Added changes and error messages to comment.validator.ts

--- backend/src/apis/comment/comment.validator.ts
+++ backend/src/apis/comment/comment.validator.ts
@@ -1,11 +1,22 @@
 import { z } from 'zod';
 
 export const CommentSchema = z.object({
-    commentId: z.number().optional(), // Assuming auto-increment in DB, optional when inserting
-    commentProfileId: z.number(),
-    commentArticleId: z.number(),
-    commentContent: z.string(),
-    commentDateTime: z.date().optional(), // Automatically set by the database, optional when inserting
+
+    commentId: z.string({required_error:"Please provide a valid commentId"})
+        .uuid({message:"Please provide a valid uuid for commentId"}),
+
+    commentProfileId: z.string({required_error:'Please provide a valid commentProfileId'})
+        .uuid({message:'Please provide a valid uuid for commentProfileId'}),
+
+    commentArticleId: z.string({required_error:'Please provide a valid commentArticleId'})
+        .uuid({message:'Please provide a valid uuid for commentArticleId'}),
+
+    commentContent: z.string({required_error:'Please provide valid commentContent'})
+        .max(512, {message: 'Max amount of characters surpassed'})
+        .min(1, {message: 'please provide a'}),
+
+    commentDateTime: z.coerce.date({required_error: 'please provide a valid commentDatetime or null'})
+        .nullable(),
 });
 
 export type Comment = z.infer<typeof CommentSchema>;
