59f6c344b97131a83a4b6bc68132030b0a2cd59b
Owen Scarpa <154032546+odscarpa@users.noreply.github.com>
2024-03-13 14:09:23 -0600

    changed login directory and function to sign in and changing profile page view

--- frontend/src/app/profile-page/page.tsx
+++ frontend/src/app/profile-page/page.tsx
@@ -36,33 +36,16 @@ export default function Profile() : JSX.Element {
                     </div>
                 </div>
 
-                {/* edit profile */}
-                <div className='grid'>
+                {/* follow profile */}
+                <div className='grid pt-5 max-w-full md:w-96 lg:w-96'>
 
-                    {/* edit profile */}
-                    <ul className="menu menu-horizontal z-50">
-                        <li>
-                            <details>
-
-                                {/* edit dropdown */}
-                                <summary className="text-black text-right text-6xl focus:outline-2 cursor-pointer">
-                                    . . .
-                                </summary>
-
-                                {/* Drop down links */}
-                                <ul className="bg-white italic p-2 rounded-t-none text-black">
-
-                                    {/* Edit item */}
-                                    <li><a href='../profile-edit-page'>Edit Profile</a></li>
-                                </ul>
-                            </details>
-                        </li>
-                    </ul>
+                    {/* follow profile */}
+                    <button className="btn btn-active bg-[#2eff9a] text-[#fffde3] hover:bg">Follow +</button>
                 </div>
             </section>
 
             <section className='my-24'>
-                <div className="container mx-auto">
+            <div className="container mx-auto">
                     <div className='flex justify-between'>
                         <h2 className='text-black text-3xl font-custom md:text-4xl lg:text-5xl'>Liked Articles</h2>
                     </div>
